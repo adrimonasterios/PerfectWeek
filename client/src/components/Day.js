d7e84aa4fcf927c570d085e0c2bf7bf126b06864
Adrian <adri.suscripciones@gmail.com>
2018-10-17 12:32:21 -0400

    Day numbers

--- client/src/components/Day.js
+++ client/src/components/Day.js
@@ -5,7 +5,7 @@ class Day extends React.Component{
   render(){
     return(
       <div className="day_table">
-        <p>Day</p>
+        <p>Day {this.props.joined +1}</p>
         <div onClick={(ev)=>{ev.preventDefault(); this.props.currentDay(this.props.joined)}}>
           <Link to={`/week/currentday`}>{this.props.restaurants}</Link>
         </div>
