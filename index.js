65e0edb3f3dbd4747e40d65e51d06834af2b9197
Adrian <adri.suscripciones@gmail.com>
2018-10-17 16:00:08 -0400

    Ready

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 const yelp = require('yelp-fusion');
 const apiKey = 'Bearer zMhGgLW70Ydy2AzBdWa9moBjqx0rI6oe8on2X34CI8CgdSOJi9EMxaopOtmU9Ovs2-QYJSJNrBhRRB2vqELGvy5PUruzjgWumWxKmnx5AhtXQi6cQcvgOVVxK8jCW3Yx';
 const categories = require('./categories.json');
+const path = require('path');
 
 
 
