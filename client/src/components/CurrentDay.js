7a814e328d1efb0e89fc6285c561fc435b11a887
Adrian <adri.suscripciones@gmail.com>
2018-10-16 20:52:28 -0400

    day site ready

--- client/src/components/CurrentDay.js
+++ client/src/components/CurrentDay.js
@@ -4,7 +4,32 @@ class CurrentDay extends React.Component{
   render(){
     return(
       <div>
-        <h1>{`${this.props.sevenRes()[this.props.current]}`}</h1>
+        <p>Your Restaurant today:</p>
+        {this.props.sevenRes()[this.props.current] &&
+          <h1>{this.props.sevenRes()[this.props.current].name}</h1>}
+        {this.props.sevenRes()[this.props.current] &&
+          <img src={`${this.props.sevenRes()[this.props.current].image_url}`} alt={`Photo of restaurant`}/>}
+        {this.props.sevenRes()[this.props.current] &&
+          <p><span>Address: </span>{this.props.sevenRes()[this.props.current].location.display_address[0]},</p>}
+        {this.props.sevenRes()[this.props.current] &&
+          <p> {this.props.sevenRes()[this.props.current].location.display_address[1]}</p>}
+        {this.props.sevenRes()[this.props.current] &&
+          <p><span>Phone: </span>{this.props.sevenRes()[this.props.current].display_phone}</p>}
+        {this.props.sevenRes()[this.props.current] &&
+          <p><a href={`${this.props.sevenRes()[this.props.current].url}`}><span>Yelp Reviews</span></a></p>}
+        <p>Entertainment for today:</p>
+        {this.props.sevenActive()[this.props.current] &&
+          <h1>{this.props.sevenActive()[this.props.current].name}</h1>}
+        {this.props.sevenActive()[this.props.current] &&
+          <img src={`${this.props.sevenActive()[this.props.current].image_url}`} alt={`Photo of entertainment`}/>}
+        {this.props.sevenActive()[this.props.current] &&
+          <p><span>Address: </span>{this.props.sevenActive()[this.props.current].location.display_address[0]},</p>}
+        {this.props.sevenActive()[this.props.current] &&
+          <p> {this.props.sevenActive()[this.props.current].location.display_address[1]}</p>}
+        {this.props.sevenActive()[this.props.current] &&
+          <p><span>Phone: </span>{this.props.sevenRes()[this.props.current].display_phone}</p>}
+        {this.props.sevenActive()[this.props.current] &&
+          <p><a href={`${this.props.sevenActive()[this.props.current].url}`}><span>Yelp Reviews</span></a></p>}
       </div>
     )
   }
--- client/src/components/Week.js
+++ client/src/components/Week.js
@@ -3,47 +3,16 @@ import Day from "./Day";
 
 
 class Week extends React.Component {
-  // sevenRes (){
-  //   let otherArr = this.props.queryData;
-  //   let elementArr = otherArr.filter((element)=> element.review_count>1000);
-  //   elementArr.sort((a,b) => {
-  //     return b.rating-a.rating
-  //     // return ((((a.rating/a.review_count)*10000)-((b.rating/b.review_count)*10000)))
-  //   })
-  //   return elementArr.filter((element, index) => index < 7 )
-  // }
-  //
-  // sevenActive(){
-  //   let activeArr = this.props.queryDataActive;
-  //   let artsArr = this.props.queryDataArts;
-  //   let publicArr = this.props.queryDataPublicArts;
-  //   let arr1 = activeArr.concat(artsArr);
-  //   let otherArr = arr1.concat(publicArr);
-  //   let elementArr = otherArr.filter((element)=> element.review_count>600);
-  //   elementArr.sort((a,b) => {
-  //     return b.rating-a.rating
-  //     // return ((((a.rating/a.review_count)*10000)-((b.rating/b.review_count)*10000)))
-  //   })
-  //   return elementArr.filter((element, index) => index < 7 )
-  // }
-  // componentDidMount(){
-  //   let res = this.props.sevenRes()
-  //   let active = this.props.sevenActive()
-  //   this.props.update(res, active)
-  // }
-
   render(){
     let res = this.props.sevenRes()
     let active = this.props.sevenActive()
 
     return(
       <div>
-        <div><p>Restaurants!</p>
+        <div>
           {res.map((element, index)=>{
             return <Day currentDay={this.props.currentDay} restaurants={element.name} className={index} key={`key_${index}`}/>
           })}
-        </div>
-        <div><p>Entertainment!</p>
           {active.map((element, index)=>{
             return <Day currentDay={this.props.currentDay} entertainment={element.name} className={index} key={`key_${index}`}/>
           })}
