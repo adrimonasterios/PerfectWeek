e10890a44523be4724e1c8c43a01f1d09a871b23
Adrian <adri.suscripciones@gmail.com>
2018-10-17 15:58:15 -0400

    Ready

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get('/home4', async (req, res) => {
   res.json(data);
 })
 
-routes.get('/', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile( path.join(__dirname, './client/build/index.html') );
 })
 
